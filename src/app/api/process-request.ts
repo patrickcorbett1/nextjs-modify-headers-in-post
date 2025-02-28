879392e67c47516dfb1a27cfaf235a77e8fa1216
Patrick Corbett <patrick.corbett@morpheuswealth.com>
2025-02-28 11:59:03 +1100

    Add another example

--- src/app/api/process-request.ts
+++ src/app/api/process-request.ts
@@ -1,7 +1,11 @@
 import { NextRequest, NextResponse } from "next/server"
+import { headers } from "next/headers";
 
 export async function processRequest(req: NextRequest) {
     console.log("POST request received")
+    const foundHeaders = await headers()
+    console.log("Read headers")
+    console.log(JSON.stringify(foundHeaders, null, 2))
     const body = await req.json()
     console.log("Body parsed")
     console.log(JSON.stringify(body, null, 2))
--- src/middleware.ts
+++ src/middleware.ts
@@ -2,13 +2,16 @@ import { NextMiddleware, NextResponse } from "next/server";
 
 export const middleware: NextMiddleware = (req) => {
   if (req.method === "POST" && req.url.includes("post-failure")) {
-    const headers = new Headers(req.headers);
-    headers.set("hello", "world");
+    console.log("Adding new header to request")
     return NextResponse.next({
         request: {
-        headers: headers,
-        },
+            headers: new Headers({
+              ...req.headers,
+              "hello": "world",
+            }),
+          }
     });
-  }
+  }      
+
   return NextResponse.next();
 }
